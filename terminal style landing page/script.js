a23b68cfdb8e760359dd0bd1ed5c7ab34189a131
VictorDepor <160658377+VictorDepor@users.noreply.github.com>
2025-08-09 20:51:49 +0200

    Update script.js

--- terminal style landing page/script.js
+++ terminal style landing page/script.js
@@ -2,7 +2,10 @@ const about = document.querySelector("#about");
 const contact = document.querySelector("#contact");
 const aboutContent = document.querySelector("#about-content");
 const contactContent = document.querySelector("#contact-content");
-
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
+const aboutContent = document.querySelector("#about-content");
+const contactContent = document.querySelector("#contact-content");
 about.addEventListener("click", () => {
   const aboutBox = new WinBox({
     title: "About Me",
